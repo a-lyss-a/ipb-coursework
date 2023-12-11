e7045546d82b06899e391f7dc76a1e64aab2c91a
SzarkaBence <eg20159@bristol.ac.uk>
2023-12-11 12:23:30 +0000

    working now

--- predictive_coding.py
+++ predictive_coding.py
@@ -13,7 +13,8 @@ class PEUnit:
         
         self.r = np.zeros((r_size, 1))
         self.U = np.random.rand(I_size, r_size)
-        self.V = np.random.rand(r_size, r_size)
+        self.V = np.identity(5)
+        #self.V = np.random.rand(r_size, r_size)
         # learning rates
         self.alpha = alpha
         self.beta = beta
@@ -32,17 +33,18 @@ class PEUnit:
         # error from bottom-up
         err_bu = I - np.matmul(self.U, self.r) #1x1
         # r-hat post-Kalman filter
+        cursed_r = np.matmul(np.linalg.inv(self.V), self.r)
         r_hat = (self.r
                  + self.alpha * (self.U.T * err_bu)
-                 + self.beta * err_td
-                 - self.gamma * self.r)
+                 + self.beta * err_td)
+                 #- self.gamma * self.r)
 
         # transition matrix from previous timestep to now
-        new_V = self.V + self.epsilon * np.matmul( (r_hat - self.r), r_hat.T ) - self.eta * self.V
+        new_V = self.V + self.epsilon * np.matmul( (r_hat - self.r), r_hat.T )# - self.eta * self.V
         # generative matrix
-        new_U = self.U + self.zeta * ( (I - np.matmul(self.U, r_hat)) * r_hat.T ) - self.theta * self.U
+        new_U = self.U + self.zeta * ( (I - np.matmul(self.U, r_hat)) * r_hat.T ) #- self.theta * self.U
 
-        self.r = np.tanh( np.matmul(self.V, r_hat) ) + self.noise.normal(scale=0.05)
+        self.r = np.tanh( np.matmul(self.V, r_hat) )# + self.noise.normal(scale=0.005)
         self.V = new_V
         self.U = new_U
 
@@ -66,12 +68,13 @@ class Network:
         # noise generator
         self.noise = np.random.default_rng()
         # define the units and their outputs
-        self.layer1 = [PEUnit(5, 1, 0.5, 0.5, 0.001, 0.1, 0.1, 0.1, 0.1)]
-        self.layer2 = [PEUnit(5, 1, 0.5, 0.5, 0.001, 0.1, 0.1, 0.1, 0.1)]
+        self.layer1 = [PEUnit(5, 1, 0.1, 0.1, 0.001, 0.1, 0.1, 0.1, 0.1)]
+        self.layer2 = [PEUnit(5, 1, 0.1, 0.1, 0.001, 0.1, 0.1, 0.1, 0.1)]
 
     def generate_input(self, out_type = 0):
-        if(out_type == 0): out = np.sin(self.step) + self.noise.normal(scale=0.05)
-        if(out_type == 1): out = np.sin(self.step)    
+        step_forward = self.step/10
+        if(out_type == 0): out = np.sin(step_forward) + self.noise.normal(scale=0.005) #was 0.05
+        if(out_type == 1): out = np.sin(step_forward)    
         if(out_type == 2): out = 0.5
         return out
     
@@ -90,7 +93,7 @@ class Network:
             self.step += 1
 
             # generate input
-            It = self.generate_input(out_type= 2)
+            It = self.generate_input(out_type= 0)
 
             # update everything and put the predictions into an array
             for i in range(len(self.layer1)):
@@ -106,4 +109,6 @@ class Network:
                 print(f"input: {It}, layer1: {preds_12}, layer2: {preds_23}")
 
 # i forgot how the shell works
-main(20)
+rnaaaaa = np.random.default_rng()
+random_length = np.floor(rnaaaaa.normal(loc = 700, scale=100))
+main(random_length)
